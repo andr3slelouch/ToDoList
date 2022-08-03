feed97dba54f889a64634d4bb2ab295315ff3f52
andr3slelouch <luis.andradec14@gmail.com>
2022-08-03 18:52:57 -0500

    Updates in repo

--- ToDoListBackend/src/main/java/org/example/model/ToDoList.java
+++ ToDoListBackend/src/main/java/org/example/model/ToDoList.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import org.jboss.logging.Logger;
 @Data
 public class ToDoList {
-    Logger logger = Logger.getLogger(ToDoList.class);
     private String title;
     private String description;
     private String state;
@@ -15,6 +14,7 @@ public class ToDoList {
      * @param state State to set to the object
      */
     public void setState(String state){
+        Logger logger = Logger.getLogger(ToDoList.class);
         logger.info("Setting state and forcing it to be \"DONE\" or \"INCOMPLETE\" only");
         if (state.equals("DONE")) {
             this.state = "DONE";
@@ -31,8 +31,13 @@ public class ToDoList {
      * @param state State for the object
      */
     public ToDoList(String title, String description, String state) {
+        Logger logger = Logger.getLogger(ToDoList.class);
+        logger.info("Initializing class with the following attributes");
+        logger.info("Title:"+title);
+        logger.info("Description:"+description);
         this.title = title;
         this.description = description;
         setState(state);
+        logger.info("State:"+this.state);
     }
 }
